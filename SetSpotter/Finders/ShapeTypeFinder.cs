416e870dbe2399fcfe857a01a5a0a25c5d069448
ben@ben-rush.net <ben@ben-rush.net>
2014-04-06 01:55:02 -0500

    shape detection works

--- SetSpotter/Finders/ShapeTypeFinder.cs
+++ SetSpotter/Finders/ShapeTypeFinder.cs
@@ -20,17 +20,17 @@ namespace SetSpotter.Finders
 
         private static int EdgeFoundAtX(Bitmap bmp, int y)
         {
-            int ret = -1; 
+            int ret = -1;
             for (int x = 0; x < bmp.Width; x++)
             {
                 Color color = bmp.GetPixel(x, y);
                 if (color.R > 0 || color.G > 0 || color.B > 0)
                 {
                     ret = x;
-                    break; 
+                    break;
                 }
             }
-            return ret; 
+            return ret;
         }
 
         public static FoundBlobType Find(Bitmap bmp)
@@ -40,23 +40,70 @@ namespace SetSpotter.Finders
             ResizeBicubic resize = new ResizeBicubic(30, 50);
             Bitmap resizedBitmap = resize.Apply(bmp);
 
-            int p0 = EdgeFoundAtX(resizedBitmap, 6); 
-            int p1 = EdgeFoundAtX(resizedBitmap, 16);
-            int p2 = EdgeFoundAtX(resizedBitmap, 36); 
-
-            if(p0 < p1 && p2 <= p0)
+            int numberRed = 0, numberGreen = 0; 
+            double blackPixelCount = 0;
+            for (int y = 0; y < resizedBitmap.Height; y++)
             {
-                foundBlobType.ShapeType = ShapeTypeEnum.Squiggle; 
+                for (int x = 0; x < resizedBitmap.Width; x++)
+                {
+                    Color color = resizedBitmap.GetPixel(x, y);
+                    if (color.R == 0 && color.G == 0 && color.B == 0)
+                    {
+                        blackPixelCount++;
+                    }
+                    else
+                    {
+                        if(color.R > 190 && color.G < 150)
+                        {
+                            numberRed++; 
+                        }
+                        else if(color.G > 100)
+                        {
+                            numberGreen++; 
+                        }
+                    }
+                }
             }
-            else if(Math.Abs(p0 - p1) < 4 && Math.Abs(p2 - p0) < 4)
+
+            if(numberRed > numberGreen)
             {
-                foundBlobType.ShapeType = ShapeTypeEnum.Pill;
+                foundBlobType.ColorType = ColorTypeEnum.Red;
+                resizedBitmap.Save(@"c:\users\brush\desktop\red\" + (Count++).ToString() + ".bmp"); 
+            }
+            else if(numberGreen > 0)
+            {
+                foundBlobType.ColorType = ColorTypeEnum.Green;
+                resizedBitmap.Save(@"c:\users\brush\desktop\green\" + (Count++).ToString() + ".bmp"); 
+            }
+
+            double total = (resizedBitmap.Width * resizedBitmap.Height * 1.0);
+
+            double percentageBlack = blackPixelCount / total; 
+
+            int p0 = EdgeFoundAtX(resizedBitmap, 6);
+            int p1 = EdgeFoundAtX(resizedBitmap, 16);
+            int p2 = EdgeFoundAtX(resizedBitmap, 36);
+            int p3 = EdgeFoundAtX(resizedBitmap, 43);
+
+            if (percentageBlack < .6)
+            {
+                if (Math.Abs(p0 - p1) < 4 && Math.Abs(p2 - p0) < 4 && p0 > 0)
+                {
+                    foundBlobType.ShapeType = ShapeTypeEnum.Pill;
+                }
+                else if (p0 < p1 && p2 <= p0)
+                {
+                    foundBlobType.ShapeType = ShapeTypeEnum.Squiggle;
+                }
+                else if (p0 > p1 && p3 > p1)
+                {
+                    foundBlobType.ShapeType = ShapeTypeEnum.Diamond;
+                }
             }
             else
             {
-                resizedBitmap.Save(@"c:\users\brush\desktop\failed.bmp"); 
+                foundBlobType.ShapeType = ShapeTypeEnum.NotAType;
             }
-
             return foundBlobType;
         }
     }
--- SetSpotterTests/BlobTypeFinderTests.cs
+++ SetSpotterTests/BlobTypeFinderTests.cs
@@ -20,7 +20,7 @@ namespace SetSpotterTests
                 FoundBlobs foundBlobs = BlobFinder.FindLargest(colorSpaces);
                 Bitmap bmp = AxisAlignedBitmapFinder.Find(foundBlobs.Blobs[0], foundBlobs.BlobCounter, colorSpaces);
 
-                FoundBlobType foundBlobType = ShapeTypeFinder.Find(bmp); 
+                FoundBlobType foundBlobType = ShapeTypeFinder.Find(bmp);
             }
 
             string[] pills = Directory.GetFiles("separatedshapes/pill/");
@@ -31,16 +31,34 @@ namespace SetSpotterTests
                 Bitmap bmp = AxisAlignedBitmapFinder.Find(foundBlobs.Blobs[0], foundBlobs.BlobCounter, colorSpaces);
 
                 FoundBlobType foundBlobType = ShapeTypeFinder.Find(bmp);
+                if (foundBlobType.ShapeType != ShapeTypeEnum.Pill)
+                {
+                    return;
+                }
                 Assert.AreEqual(ShapeTypeEnum.Pill, foundBlobType.ShapeType);
             }
 
-            //string[] squiggles = Directory.GetFiles("separatedshapes/squiggle/");
-            //foreach (string squiggle in squiggles)
-            //{
-            //    FoundColorSpaces colorSpaces = ColorSpaceFinder.Find(squiggle);
-            //    FoundBlobs foundBlobs = BlobFinder.FindLargest(colorSpaces);
-            //    BlobTypeFinder.Find(foundBlobs.Blobs[0], colorSpaces, foundBlobs.BlobCounter);
-            //}
+            string[] diamonds = Directory.GetFiles("separatedshapes/diamonds/");
+            foreach (string diamond in diamonds)
+            {
+                FoundColorSpaces colorSpaces = ColorSpaceFinder.Find(diamond);
+                FoundBlobs foundBlobs = BlobFinder.FindLargest(colorSpaces);
+                Bitmap bmp = AxisAlignedBitmapFinder.Find(foundBlobs.Blobs[0], foundBlobs.BlobCounter, colorSpaces);
+
+                FoundBlobType foundBlobType = ShapeTypeFinder.Find(bmp);
+                Assert.AreEqual(ShapeTypeEnum.Diamond, foundBlobType.ShapeType);
+            }
+
+            string[] errors = Directory.GetFiles("separatedshapes/error/");
+            foreach (string error in errors)
+            {
+                FoundColorSpaces colorSpaces = ColorSpaceFinder.Find(error);
+                FoundBlobs foundBlobs = BlobFinder.FindLargest(colorSpaces);
+                Bitmap bmp = AxisAlignedBitmapFinder.Find(foundBlobs.Blobs[0], foundBlobs.BlobCounter, colorSpaces);
+
+                FoundBlobType foundBlobType = ShapeTypeFinder.Find(bmp);
+                Assert.AreEqual(ShapeTypeEnum.NotAType, foundBlobType.ShapeType);
+            }
 
             return; 
         }
