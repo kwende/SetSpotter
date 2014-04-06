0e96a7a416bc2adf68c51c908f658d45f26e69d4
ben@ben-rush.net <ben@ben-rush.net>
2014-04-06 14:16:50 -0500

    fixed an image. started the color tests.

---
+++ ColorInspector/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColorInspector
+{
+    class Program
+    {
+        static void DumpHistogram()
+        {
+            string[] files = Directory.GetFiles(@"C:\Users\brush\Documents\Visual Studio 2012\Projects\SetSpotter\SetSpotter\separatedshapes\diamonds");
+            Dictionary<int, int> histogram = new Dictionary<int, int>();
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                if (fileName.StartsWith("purple"))
+                {
+                    using (Bitmap bmp = (Bitmap)Bitmap.FromFile(file))
+                    {
+                        for (int y = 0; y < bmp.Height; y++)
+                        {
+                            for (int x = 0; x < bmp.Width; x++)
+                            {
+                                int hue = (int)(bmp.GetPixel(x, y).GetHue());
+                                if (histogram.ContainsKey(hue))
+                                {
+                                    histogram[hue]++;
+                                }
+                                else
+                                {
+                                    histogram[hue] = 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            for (int c = 0; c < 360; c++)
+            {
+                if (histogram.ContainsKey(c))
+                {
+                    File.AppendAllText(@"c:\users\brush\desktop\purplefile.csv", histogram[c].ToString() + "\r\n");
+                }
+                else
+                {
+                    File.AppendAllText(@"c:\users\brush\desktop\purplefile.csv", "0\r\n");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- SetSpotterTests/BlobTypeFinderTests.cs
+++ SetSpotterTests/BlobTypeFinderTests.cs
@@ -4,14 +4,102 @@ using System.IO;
 using SetSpotter.Finders;
 using SetSpotter.FoundData;
 using System.Drawing;
+using System.Collections.Generic;
 
 namespace SetSpotterTests
 {
     [TestClass]
     public class BlobTypeFinderTests
     {
+        private int NumberInRange(Dictionary<int,int> histogram, int startRange, int endRange)
+        {
+            int ret = 0; 
+            for(int c=startRange;c<=endRange;c++)
+            {
+                if(histogram.ContainsKey(c))
+                {
+                    ret += histogram[c]; 
+                }
+            }
+            return ret; 
+        }
+
         [TestMethod]
-        public void TestBlobFinder()
+        public void TestColors()
+        {
+            string[] squiggles = Directory.GetFiles("separatedshapes/squiggle/");
+            string[] pills = Directory.GetFiles("separatedshapes/pill/");
+            string[] diamonds = Directory.GetFiles("separatedshapes/diamonds/");
+
+            List<string> all = new List<string>();
+            all.AddRange(squiggles);
+            all.AddRange(pills);
+            all.AddRange(diamonds);
+
+            foreach (string file in all)
+            {
+                string fileName = Path.GetFileName(file);
+                string colorName = fileName.Substring(0, fileName.IndexOf('_')).ToLower();
+                Dictionary<int, int> histogram = new Dictionary<int, int>();
+
+                using (Bitmap bmp = (Bitmap)Bitmap.FromFile(file))
+                {
+                    double total = 0; 
+                    for (int y = 0; y < bmp.Height; y++)
+                    {
+                        for (int x = 0; x < bmp.Width; x++)
+                        {
+                            Color color = bmp.GetPixel(x, y);
+                            int hue = (int)color.GetHue();
+                            float saturation = color.GetSaturation(); 
+                            if(saturation > .15)
+                            {
+                                total++; 
+                                if (!histogram.ContainsKey(hue))
+                                {
+                                    histogram.Add(hue, 1);
+                                }
+                                else
+                                {
+                                    histogram[hue]++;
+                                }
+                            }
+                        }
+                    }
+
+                    if (colorName == "red")
+                    {
+                        int numberInRedRange = NumberInRange(histogram, 0, 8);
+                        numberInRedRange += NumberInRange(histogram, 330, 360); 
+
+                        double percentage = numberInRedRange / total;
+
+                        //Assert.IsTrue(percentage > .5); 
+                        if(percentage < .5)
+                        {
+                            File.Delete(@"c:\users\brush\desktop\red.csv");
+                            File.Delete(@"c:\users\brush\desktop\error.bmp");
+                            File.Copy(file, @"c:\users\brush\desktop\error.bmp"); 
+                            for (int c = 0; c < 360; c++)
+                            {
+                                if (histogram.ContainsKey(c))
+                                {
+                                    File.AppendAllText(@"c:\users\brush\desktop\red.csv", histogram[c].ToString() + "\r\n");
+                                }
+                                else
+                                {
+                                    File.AppendAllText(@"c:\users\brush\desktop\red.csv", "0\r\n");
+                                }
+                            }
+                            return; 
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestShapes()
         {
             string[] diamondBmps = Directory.GetFiles("separatedshapes/squiggle/");
             foreach (string diamondBmp in diamondBmps)
@@ -60,7 +148,7 @@ namespace SetSpotterTests
                 Assert.AreEqual(ShapeTypeEnum.NotAType, foundBlobType.ShapeType);
             }
 
-            return; 
+            return;
         }
     }
 }
